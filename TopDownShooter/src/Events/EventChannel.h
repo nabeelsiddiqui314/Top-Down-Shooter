9ba2b1c4f18413fa66eb9b3118d78c4cfd9afa57
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2020-04-07 23:24:50 +0530

    Added the ability to unsubscribe from events.

--- TopDownShooter/src/Events/EventChannel.h
+++ TopDownShooter/src/Events/EventChannel.h
@@ -1,31 +1,75 @@
 #pragma once
-#include <vector>
+#include <tuple>
+#include <unordered_map>
 #include <functional>
+#include <stdint.h>
+#include <assert.h>
+
+using Subscription_ID = std::uint32_t;
+
+template <typename T>
+using Callback = std::function<void(const T&)>;
+
+template <typename T>
+class SubscriptionList {
+public:
+	SubscriptionList() = default;
+	~SubscriptionList() = default;
+public:
+	Subscription_ID addSubscriber(const Callback<T>& callback) {
+		m_callbackMap.emplace(std::make_pair(m_lastID, callback));
+		return m_lastID++; // *increments after returning
+	}
+
+	void removeAllSubscribers() {
+		m_callbackMap.clear();
+	}
+
+	void removeSubscriber(Subscription_ID id) {
+		auto iterator = m_callbackMap.find(id);
+		assert(iterator != m_callbackMap.end(), "Ivalid subscription ID");
+
+		m_callbackMap.erase(iterator);
+	}
+
+	void invoke(const T& event) {
+		for (auto& subscriber : m_callbackMap) {
+			subscriber.second(event);
+		}
+	}
+private:
+	Subscription_ID m_lastID = 0;
+	std::unordered_map<Subscription_ID, Callback<T>> m_callbackMap;
+};
 
 template <typename... Events>
-class EventChannel {
-private:
-	template <typename T>
-	using Callback = std::function<void(const T&)>;
-
-	template <typename T>
-	using SubscriptionList = std::vector<Callback<T>>;
+class EventChannel {	
 public:
 	EventChannel() = default;
 	~EventChannel() = default;
 public:
 	template <typename T>
-	void subscribe(const Callback<T>& callback) {
-		auto& subscriber = std::get<SubscriptionList<T>>(m_events);
-		subscriber.emplace_back(callback);
+	Subscription_ID subscribe(const Callback<T>& callback) {
+		auto& subscribers = std::get<SubscriptionList<T>>(m_events);
+		return subscribers.addSubscriber(callback);
 	}
 
 	template <typename T>
-	void broadcast(const T& event) {
+	void unsubscribe() {
 		auto& subscribers = std::get<SubscriptionList<T>>(m_events);
-		for (auto& subscriber : subscribers) {
-			subscriber(event);
-		}
+		subscribers.removeAllSubscribers();
+	}
+
+	template <typename T>
+	void unsubscribe(Subscription_ID id) {
+		auto& subscribers = std::get<SubscriptionList<T>>(m_events);
+		subscribers.removeSubscriber(id);
+	}
+
+	template <typename T>
+	void publish(const T& event) {
+		auto& subscribers = std::get<SubscriptionList<T>>(m_events);
+		subscribers.invoke(event);
 	}
 private:
 	std::tuple<SubscriptionList<Events>...> m_events;
